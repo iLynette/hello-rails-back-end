9fc9991e3aa54ee8b4a1f6d673ac183c26258d7d
iLynette <lynetteacholah@icloud.com>
2022-07-13 12:50:56 +0300

    Move the data to seeds

--- db/migrate/20220712192218_create_greetings.rb
+++ db/migrate/20220712192218_create_greetings.rb
@@ -5,10 +5,5 @@ class CreateGreetings < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-    Greeting.create(name: 'Hello')
-    Greeting.create(name: 'Sasa')
-    Greeting.create(name: 'Mambo')
-    Greeting.create(name: 'Poa')
-    Greeting.create(name: 'Mzuri')
   end
 end
